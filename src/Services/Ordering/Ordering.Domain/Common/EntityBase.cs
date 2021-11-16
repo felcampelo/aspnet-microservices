96bbfacfb521df304a410e6077ba7a48ccd81980
Felipe Campelo <felipe@boldtech.com.br>
2021-11-16 11:10:38 -0300

    Criada camadas.

---
+++ src/Services/Ordering/Ordering.Domain/Common/EntityBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Ordering.Domain.Common
+{
+    public abstract class EntityBase
+    {
+        public int Id { get; protected set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string LastModifiedBy { get; set; }
+        public DateTime? LasModifiedDate { get; set; }
+    }
+}
